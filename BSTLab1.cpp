21401fc935b2ba9945e5976534633832c76c3b8f
cs14af <alejandro.fernandez05@utrgv.edu>
2024-12-09 05:46:59 +0000

    lab10

--- BSTLab1.cpp
+++ BSTLab1.cpp
@@ -9,10 +9,25 @@ class Node {
 		// 	- an integer "key" that holds the value of the Node
 		// 	- a pointer to each child possible child node ("right" and "left")
 
+			int key;
 
+			Node *left;
+
+			Node * right;
 		// Write a constructor for the Node
 		// input: only an integer value for "key", no pointer arguments
 		// 		right and left pointers should be null
+		Node(int data)
+
+		{
+
+			this->key=data;
+
+			this->left=nullptr;
+
+			this->right=nullptr;
+
+		}
 
 };
 
@@ -23,22 +38,33 @@ Node* createTree() {
 	// root
 	Node *root = nullptr;
 
+	Node *root2 = new Node(13);
 	// level 1 (children of root)
+	root->left=new Node(0);
 
+	root->right=new Node(21);
 	// level 2 (children of 0)
+	root->left->right=new Node(7);
 
+	root->left->left=new Node(-2);
 	// level 2 (children of 21)
+	root->right->left=new Node(15);
 
+	root->right->right=new Node(99);
 	// level 3 (children of -2)
-
+	root->left->left->right=new Node(-1);
 	// level 3 (children of 7)
+	root->left->right->left=new Node(6);
 
+	root->left->right->right=new Node(12);
 	// level 3 (children of 15)
-
+	root->right->left->right=new Node(18);
 	// level 3 (children of 99)
+ 	root->right->right->left=nullptr;
 
+	root->right->right->right=nullptr;
 	// level 4 (children of 18)
-
+	root->right->left->right->right=new Node(19);
 	return root;
 }
 
@@ -48,13 +74,32 @@ Node* createTree() {
 // 	returns: true or false depending on if the value is found
 // You should write this function recursively! What is the base case? What is the general case?
 bool searchTree(int target, Node* root) {
-	// Base cases
+	if(root==nullptr)
 	return false;
 
 	// General case
-	return false;
-	
+	if(root->key==target)
+
+  return true;
+
+ 	else if(root->key<target)
+
+ 	{
+
+	return searchTree(target,root->right);
+
+	}	
+
+	else{
+
+	return searchTree(target,root->left);
+
+	}		
+
 }
+	
+	
+
 
 // Write a function that will return the total number of Nodes in the tree
 // 	input: pointer to the root Node
@@ -62,18 +107,29 @@ bool searchTree(int target, Node* root) {
 // You should write this function recursively!
 int treeSize(Node* root) {
 	// base case
-	return -1;
+	if(root==NULL)
 
+	return 0;
+	
+		else
+
+		return 1+treeSize(root->left)+treeSize(root->right);
 	// General case
-	return -1;
+	
 
 }
 
 // BONUS! Write a function that will determine the height of the tree
 int treeHeight(Node* root) {
 	// base case
-	return -1;
+	if(root==NULL)
+
+	return 0;
+	
 
 	// General case
-	return -1;
+	else
+
+	return 1+max(treeHeight(root->left),treeHeight(root->right));
+	
 }
